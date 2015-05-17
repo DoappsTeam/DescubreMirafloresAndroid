75d8e0787490ce360a5c04350d9fc55d73cd7443
william_ST <sulca.03.1995@gmail.com>
2015-05-17 08:39:38 -0500

    title view indicator

---
+++ app/src/main/java/me/doapps/miraflores/helper/Movie.java
@@ -0,0 +1,17 @@
+package me.doapps.miraflores.helper;
+
+/**
+ * Created by william on 17/05/2015.
+ */
+public class Movie {
+    public int id;
+    public String title;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String title) {
+        this.title = title;
+        this.id = id;
+    }
+}
