19fd935cb3743c4ab35cd24d59b95cf77696082f
Jonathan Nolasco <jnolascob@gmail.com>
2015-05-17 06:33:09 -0500

    update settings fragment

--- app/src/main/java/me/doapps/miraflores/fragments/SettingsFragment.java
+++ app/src/main/java/me/doapps/miraflores/fragments/SettingsFragment.java
@@ -6,25 +6,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import me.doapps.miraflores.R;
 
 /**
  * Created by william on 17/05/2015.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+    private ImageView imgMuseum;
+    private ImageView imgFood;
+    private ImageView imgCulture;
+    private ImageView imgPark;
+    private ImageView imgTourist;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
-
+        imgMuseum = (ImageView)view.findViewById(R.id.imgMuseum);
+        imgFood = (ImageView)view.findViewById(R.id.imgFood);
+        imgCulture = (ImageView)view.findViewById(R.id.imgCulture);
+        imgPark = (ImageView)view.findViewById(R.id.imgPark);
+        imgTourist = (ImageView)view.findViewById(R.id.imgTourist);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        imgMuseum.setOnClickListener(this);
+        imgFood.setOnClickListener(this);
+        imgCulture.setOnClickListener(this);
+        imgPark.setOnClickListener(this);
+        imgTourist.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imgMuseum:
+                imgMuseum.setImageDrawable(getResources().getDrawable(R.drawable.selector_brown_disable));
+                break;
+            case R.id.imgFood:
+                imgMuseum.setImageDrawable(getResources().getDrawable(R.drawable.selector_red_disable));
+                break;
+            case R.id.imgCulture:
+                imgMuseum.setImageDrawable(getResources().getDrawable(R.drawable.selector_green_disable));
+                break;
+            case R.id.imgPark:
+                imgMuseum.setImageDrawable(getResources().getDrawable(R.drawable.selector_yellow_disable));
+                break;
+            case R.id.imgTourist:
+                imgMuseum.setImageDrawable(getResources().getDrawable(R.drawable.selector_orange_disable));
+                break;
+            default:break;
+        }
     }
 }
 
