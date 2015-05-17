f4705ffba7222ef82feae829b7c7a81900209998
william_ST <sulca.03.1995@gmail.com>
2015-05-17 03:55:49 -0500

    design MenuActivity

---
+++ app/src/main/java/me/doapps/miraflores/fragments/OrderedFragment.java
@@ -0,0 +1,30 @@
+package me.doapps.miraflores.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.doapps.miraflores.R;
+
+/**
+ * Created by william on 17/05/2015.
+ */
+public class OrderedFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ordered, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
+
